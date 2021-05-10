a689ef733478ab23a5f75cd02b5bc608c6328cb5
Ertuğrul TURAN <60829297+ertugrulturan@users.noreply.github.com>
2021-05-10 20:39:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@
 # #TS3/5 IP : NinjaNetWork
 import os
 os.system('wget https://files.teamspeak-services.com/releases/server/3.13.3/teamspeak3-server_linux_amd64-3.13.3.tar.bz2')
-os.system('tar -xvf teamspeak3-server_linux_amd64-3.12.0.tar.bz2')
-os.system('rm teamspeak3-server_linux_amd64-3.12.0.tar.bz2')
+os.system('tar -xvf teamspeak3-server_linux_amd64-3.13.3.tar.bz2')
+os.system('rm teamspeak3-server_linux_amd64-3.13.3.tar.bz2')
 os.system('echo "license_accepted=1" > teamspeak3-server_linux_amd64/.ts3server_license_accepted')
 os.system('bash teamspeak3-server_linux_amd64/ts3server_startscript.sh start')
 print "-----------=[ \033[91mHost/\033[94m TS5 Server Kuruldu! ]=-----------"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
